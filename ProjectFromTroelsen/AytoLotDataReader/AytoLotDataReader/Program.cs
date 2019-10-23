44e72d6b1dfabdf2577a2967271d5547270a4f0d
YuraMelnyk <melnyk.yra27@gmail.com>
2019-10-23 14:45:43 +0300

    Work with DataReader

---
+++ ProjectFromTroelsen/AytoLotDataReader/AytoLotDataReader/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using static System.Console;
+
+namespace AytoLotDataReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = @"Data Source = (local)\SQLEXPRESS; Integrated Security = SSPI;" +
+                    "Initial Catalog = AutoLot";
+                connection.Open();
+
+                string sql = "Select * From Inventory";
+                SqlCommand myCommand = new SqlCommand(sql, connection);
+
+                using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+                {
+                    while (myDataReader.Read())
+                    {
+                        WriteLine($"-> Make {myDataReader["Make"]},\t PetName {myDataReader["PetName"]},\t Color {myDataReader["Color"]}");
+                    }
+                }
+            }
+            ReadLine();
+        }
+    }
+}
