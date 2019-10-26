8c356ef3f44318e9077151e2b62dc6b7048a0975
YuraMelnyk <melnyk.yra27@gmail.com>
2019-10-26 15:39:27 +0300

    Use AutoLotDAL

--- ProjectFromTroelsen/AutoLotCUIClient/AutoLotCUIClient/Program.cs
+++ ProjectFromTroelsen/AutoLotCUIClient/AutoLotCUIClient/Program.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoLotDAL;
 using System.Configuration;
+using System.Data;
+using AutoLotDAL.ConnectedLayer;
+using AutoLotDAL.Models;
+using static System.Console;
 
 namespace AutoLotCUIClient
 {
@@ -12,6 +16,58 @@ namespace AutoLotCUIClient
     {
         static void Main(string[] args)
         {
+            WriteLine("***** The AutoLot Console UI *****");
+            string connectionString = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ConnectionString;
+            bool userDone = false;
+            string userCommand = "";
+
+            InventoryDAL invDAL = new InventoryDAL();
+            invDAL.OpenConnection(connectionString);
+            try
+            {
+                ShowInstruction();
+                do
+                {
+                    Write("\nPlease enter yuor command:");
+                    userCommand = ReadLine();
+                    WriteLine();
+                    switch (userCommand?.ToUpper()??"")
+                    {
+                        case "I":
+                            InsertNewCar(invDAL);
+                            break;
+                        case "U":
+                            UpdateCarPetName(invDAL);
+                            break;
+                        case "D":
+                            DeleteCar(invDAL);
+                            break;
+                        case "L":
+                            ListInventory(invDAL);
+                            break;
+                        case "S":
+                            ShowInstructions();
+                            break;
+                        case "P":
+                            LookUpPetName(invDAL);
+                            break;
+                        case "Q":
+                            userDone = true;
+                            break;
+                        default:
+                            WriteLine("Bad data! Try again.");
+                            break;
+                    }
+                } while (!userDone);
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+            finally
+            {
+                invDAL.CloseConnectin();
+            }
         }
     }
 }
