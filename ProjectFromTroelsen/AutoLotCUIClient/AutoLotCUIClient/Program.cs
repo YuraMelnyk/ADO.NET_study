cb9e40efdd8ba09b97e978a925fa09f7478cc59c
YuraMelnyk <melnyk.yra27@gmail.com>
2019-10-26 22:35:47 +0300

    test AutoLotCUIClient

--- ProjectFromTroelsen/AutoLotCUIClient/AutoLotCUIClient/Program.cs
+++ ProjectFromTroelsen/AutoLotCUIClient/AutoLotCUIClient/Program.cs
@@ -10,6 +10,7 @@ using AutoLotDAL.ConnectedLayer;
 using AutoLotDAL.Models;
 using static System.Console;
 
+
 namespace AutoLotCUIClient
 {
     class Program
@@ -19,13 +20,13 @@ namespace AutoLotCUIClient
             WriteLine("***** The AutoLot Console UI *****");
             string connectionString = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ConnectionString;
             bool userDone = false;
-            string userCommand = "";
+            string userCommand = "";           
 
             InventoryDAL invDAL = new InventoryDAL();
             invDAL.OpenConnection(connectionString);
             try
             {
-                ShowInstruction();
+                ShowInstructions();
                 do
                 {
                     Write("\nPlease enter yuor command:");
@@ -69,5 +70,87 @@ namespace AutoLotCUIClient
                 invDAL.CloseConnectin();
             }
         }
+
+        private static void ShowInstructions()
+        {
+            WriteLine("I: Insert a new car.");
+            WriteLine("U: Updates an existing car.");
+            WriteLine("D: Delete an existing car.");
+            WriteLine("L: Lists current inventory.");
+            WriteLine("S: Shows these instructions.");
+            WriteLine("P: Looks up pet name.");
+            WriteLine("Q: Quits program.");
+        }
+
+        private static void ListInventory(InventoryDAL invDAL)
+        {
+            DataTable dt = invDAL.GetAllInventoryAsDataTable();
+            DisplyTable(dt);
+        }
+
+        private static void DisplyTable(DataTable dt)
+        {
+            for (int curCol = 0; curCol < dt.Columns.Count; curCol++)
+            {
+                Write($"{dt.Columns[curCol].ColumnName}\t");
+            }
+            WriteLine("\n------------------------------------");
+            for (int curRow = 0; curRow < dt.Rows.Count; curRow++)
+            {
+                for (int curCol = 0; curCol < dt.Columns.Count; curCol++)
+                {
+                    Write($"{dt.Rows[curRow][curCol]}\t");
+                }
+                WriteLine();
+            }
+        }
+
+        private static void DeleteCar(InventoryDAL invDAL)
+        {
+            Write("Enter ID of Car to delete:");
+            int id = int.Parse(ReadLine() ?? "0");
+            try
+            {
+                invDAL.DeleteCar(id);
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+        }
+
+        private static void InsertNewCar(InventoryDAL invDAL)
+        {
+            Write("Enter Car ID:");
+            var newCarId = int.Parse(ReadLine() ?? "0");
+            Write("Enter Car Color:");
+            var newCarColor = ReadLine();
+            Write("Enter Car Make");
+            var newCarMake = ReadLine();
+            Write("Enter Pet Name: ");
+            var newCarPetName = ReadLine();
+            invDAL.InsertAuto(newCarId, newCarColor, newCarMake, newCarPetName);
+        }
+
+        private static void UpdateCarPetName(InventoryDAL invDAL)
+        {
+            Write("Enter Car ID:");
+            var carID = int.Parse(ReadLine() ?? "0");
+            Write("Enter New Car Pet Name:");
+            var newCarPetName = ReadLine();
+            Write("Enter New Car Color:");
+            var newCarColor = ReadLine();
+            invDAL.UpdateCar(carID, newCarPetName, newCarColor);
+        }
+
+        private static void LookUpPetName(InventoryDAL invDAL)
+        {
+            Write("Enter ID of Car to look up:");
+            int id = int.Parse(ReadLine() ?? "0");
+            WriteLine($"PetName of {id} is {invDAL.LookUpPetName(id).TrimEnd()}.");
+        }
     }
+
+
+   
 }
