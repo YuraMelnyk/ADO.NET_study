d14bd14b9425e8cb3a842d7a1ab2df3757924ddb
YuraMelnyk <melnyk.yra27@gmail.com>
2019-11-01 18:17:17 +0200

    Use LINQ to DataSet

---
+++ ProjectsFromTroelsen/Disconnected Layer/LinqToDataSetApp/LinqToDataSetApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using AutoLotDAL.DataSets;
+using AutoLotDAL.DataSets.AutoLotDataSetTableAdapters;
+
+namespace LinqToDataSetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********LINQ over DataSet***********");
+            AutoLotDataSet dal = new AutoLotDataSet();
+            InventoryTableAdapter tableAdapter = new InventoryTableAdapter();
+            AutoLotDataSet.InventoryDataTable data = tableAdapter.GetData();
+
+            Console.ReadLine();
+        }
+
+        static void PrintAllCarIds(DataTable data)
+        {
+            EnumerableRowCollection enumData = data.AsEnumerable();
+
+            foreach (DataRow r in enumData)
+            {
+                Console.WriteLine($"Car ID = {r["CarId"]}");
+            }
+        }
+
+
+    }
+}
