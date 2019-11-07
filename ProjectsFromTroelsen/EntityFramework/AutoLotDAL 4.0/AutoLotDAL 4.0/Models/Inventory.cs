2a115f127d7ce300a9e0708e4a603f4aeac6ecf3
YuraMelnyk <melnyk.yra27@gmail.com>
2019-11-07 23:12:57 +0200

    AutoLotDAL 4.0 => EF + Models

---
+++ ProjectsFromTroelsen/EntityFramework/AutoLotDAL 4.0/AutoLotDAL 4.0/Models/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace AutoLotDAL_4._0.Models
+{
+    [Table("Inventory")]
+    public partial class Inventory
+    {
+        [Key]
+        public int CarId { get; set; }
+        [StringLength(50)]
+        public string Make { get; set; }
+        [StringLength(50)]
+        public string Color { get; set; }
+        [StringLength(50)]
+        public string PetName { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; } = new HashSet<Order>();
+
+        public override string ToString()
+        {
+            return $"{this.PetName ?? "**NoName**"} is a {this.Color} {this.Make} with ID {this.CarId}";
+        }
+
+        [NotMapped]
+        public string MakeColor => $"{Make}+{Color}";
+    }
+}
---
+++ ProjectsFromTroelsen/EntityFramework/AutoLotDAL 4.0/AutoLotDAL 4.0/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoLotDAL_4._0.Models
+{
+    public partial class Order
+    {
+        [Required, Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+        [Required]
+        public int CustId { get; set; }
+        public int CarId { get; set; }
+        [ForeignKey("CustId")]
+        public virtual  Customer Customer { get; set; }
+        [ForeignKey("CarId")]
+        public virtual Inventory Car { get; set; }
+    }
+}
